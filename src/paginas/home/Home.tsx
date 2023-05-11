f2fec995a3def9290f61ec69c859bd1d22d0e3f1
Jessica Mendes <jessicamend17@gmail.com>
2023-05-11 18:51:03 -0300

    criação página home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <img src="/src/assets/img/banner.jpg" alt="imagem banner" />
+    </>
+  );
+}
+
+export default Home;
